815b78c6ba715165f15abd644bc8c61173d5566f
CUONGNH <nguyenhongcuong69@gmail.com>
2019-11-08 16:04:20 +0700

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const WEBHOOK_SECRET = 'webhook secret key';
+const WEBHOOK_SECRET = 'f273cfab9dc7884b15cba428338a69bf2028f77517a7d639ffcf49f4413d252b0e0d058db438b769525ce9dc9257451d';
 const crypto = require('crypto');
 function verifySignature (body, signature) {
     const digest = crypto
@@ -11,5 +11,6 @@ app.post('/webhooks', function (req, res, next) {
     if (!verifySignature(req.rawBody, req.headers['x-tawk-signature'])) {
         // verification failed
     }
+    debugger;
     // verification success
 });
